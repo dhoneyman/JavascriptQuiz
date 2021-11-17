ea0d6574590e0640ece5af135cb4bd01fcfd0b0c
Dylan Honeyman <dylan@dylans-iMac.local>
2021-11-16 19:57:24 -0600

    reorganizing folders

--- Assets/js/highScoreScript.js
+++ Assets/js/highScoreScript.js
@@ -3,16 +3,24 @@ let scoreUl = document.querySelector('#high-score-ul');
 let newHighScore = document.querySelector('#newScore')
 let submitButton = document.querySelector('#submit');
 
+
 newHighScore.textContent = localStorage.getItem('rightScore');
 
 submitButton.addEventListener('click', function(event){
 
 event.preventDefault();
-    let newPlayer = document.getElementById('playerName').value;
-    let entry = document.createElement('li');
-    entry.textContent = newPlayer;
+    // let newPlayer = document.getElementById('playerName').value;
+    // let entry = document.createElement('li');
+    // // entry = newHighScore;
     // entry.appendChild(document.createTextNode(newPlayer));
-    scoreUl.appendChild(entry);
+    // scoreUl.appendChild(entry);
+
+    let newPlayer = document.getElementById('playerName').value;
+    playerArray.push(newHighScore, playerName);
+    
+
+
+
     
 
 // function scorePage (){
